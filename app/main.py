a0460d50ace228ededd8aba1fd60ef8d1644e803
naligant <naligant@msu.edu>
2024-10-12 19:35:38 -0400

    mongodb commented out.

--- app/main.py
+++ app/main.py
@@ -13,23 +13,23 @@ app = FastAPI()
 
 #connect mongodb database
 
-uri = os.getenv("MONGODB_URI")
-# print(uri)
+# uri = os.getenv("MONGODB_URI")
+# # print(uri)
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
-# print(client)
+# # Create a new client and connect to the server
+# client = MongoClient(uri)
+# # print(client)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+# # Send a ping to confirm a successful connection
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
 # if __name__ == '__main__':
 #     uvicorn.run(app, host="0.0.0.0", port = 8000)
