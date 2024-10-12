0abe01d1df987adb12e216f51782ec6e3e26f4c8
sammon04 <samuel.belanger19@gmail.com>
2024-10-12 19:00:58 -0400

    Added images repository, added a line in page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -5,6 +5,7 @@ import TextField from '@mui/material/TextField'
 
 export default function Home() {
   return( <div>
+    
     <Box
       component = "form"
       sx = {{ '& >:not(style)':{m:1, width: '25ch'}}}
