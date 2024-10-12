d70a025e3f509d0704014ca40e8345eaaf2f8dc1
Leo <leoeatsgrass@gmail.com>
2024-10-12 18:35:15 -0400

    Cleared global, added box to pages

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,18 @@
 import Image from "next/image";
+import * as React from 'react';
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
 
 export default function Home() {
-  
+  return( <div>
+    <Box
+      component = "form"
+      sx = {{ '& >:not(style)':{m:1, width: '25ch'}}}
+      noValidate
+      autoComplete = "off"
+      ><TextField id="standard-basic" label="Enter Ingredients" variant="standard" /></Box>
+
+  </div>
+    
+  )
 }
