11fc1de69d284c83a682a0b0d815ed0b9344d6d8
naligant <naligant@msu.edu>
2024-10-13 00:07:50 -0400

    Homepage routing fixed.

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -1,83 +1,150 @@
 "use client";
-import addIcon from '../images/add.png'
-import logoutIcon from '../images/logout.png'
-import profileIcon from '../images/profile-user.png'
 import * as React from 'react';
-import Image from 'next/image'
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import { Box } from '@mui/material';
-// import FolderIcon from '@mui/icons-material/Folder';
-// import RestoreIcon from '@mui/icons-material/Restore';
-// import FavoriteIcon from '@mui/icons-material/Favorite';
-// import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Link from 'next/link'; // Import Link from next/link
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+const pages = [
+    { name: 'Add Ingredients', href: '/add-ingredients' },
+    { name: 'Meal Calc', href: '/meal' },
+    { name: 'Logout', href: '/logout' }
+];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 export default function Home() {
-    const [value, setValue] = React.useState('recents');
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
 
-    const Card = {
-        display: 'flex',
-        flexDirection: 'row',
-        alignSelf: 'center'
-    }
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
     };
 
     return (
         <>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    padding: 2,
-                }}
-            >
-                <header>
-                    <h1>This is the Homepage</h1>
-                </header>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        padding: 2,
-                    }}>
-                    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-                    <BottomNavigationAction
-                        label="Add ingredients"
-                        value="ingredients"
-                        icon={<Image
-                            src={addIcon}
-                            alt="Add ingredients"
-                            width={35}
-                            height={35}/>}
-                    />
-                    <BottomNavigationAction
-                        label="Logout"
-                        value="logout"
-                        icon={<Image
-                            src={logoutIcon}
-                            alt="Logout"
-                            width={35}
-                            height={35}/>}
-                    />
-                    <BottomNavigationAction
-                        label="profile"
-                        value="profile"
-                        icon={<Image
-                            src={profileIcon}
-                            alt="Logout"
-                            width={35}
-                            height={35}/>}
-                    />
-                    </BottomNavigation>
-                </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <AppBar position="static" sx={{ width: '100%', backgroundColor: 'lightgray', boxShadow: 'none' }}>
+                    <Container maxWidth={false}>
+                        <Toolbar disableGutters sx={{ padding: 0 }}>
+                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleOpenNavMenu}
+                                    color="inherit"
+                                >
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorElNav}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'left',
+                                    }}
+                                    open={Boolean(anchorElNav)}
+                                    onClose={handleCloseNavMenu}
+                                    sx={{ display: { xs: 'block', md: 'none' } }}
+                                >
+                                    {pages.map((page) => (
+                                        <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                                            <Link href={page.href} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                                <Typography sx={{ textAlign: 'center' }}>{page.name}</Typography>
+                                            </Link>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                                {pages.map((page) => (
+                                    <Link key={page.name} href={page.href} style={{ textDecoration: 'none' }}>
+                                        <Button sx={{ my: 2, color: 'black', display: 'block' }}>
+                                            {page.name}
+                                        </Button>
+                                    </Link>
+                                ))}
+                            </Box>
+
+                            <Box sx={{ flexGrow: 0 }}>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                        <Avatar alt="" src="/static/images/avatar/2.jpg" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: '100%' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    {settings.map((setting) => (
+                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                            <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
             </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: 2 }}>
+                <header>
+                    <h1>Nutrition Planner</h1>
+                </header>
+                <header>
+                    <h1>About</h1>
+                </header>
+                <p>
+                    Ever found yourself staring at your pantry, unsure of what to cook with the ingredients you have? 
+                    This web app takes the guesswork out of meal planning! Just enter your ingredients, set your desired 
+                    calorie count, choose your preferred cuisine, and voilà—your custom-made recipe is ready! Say goodbye 
+                    to indecision and hello to delicious, tailored meals at home!
+                </p>
+                <header>
+                    <h1>Current Ingredients</h1>
+                </header>
+                <header>
+                    <h1>Previous Recipes</h1>
+                </header>
+            </Box>
+
         </>
     );
 }
