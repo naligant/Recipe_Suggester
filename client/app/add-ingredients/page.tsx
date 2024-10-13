1c0021b6363556797ba8f6b9b8c8c03d7be20f51
Leo <leoeatsgrass@gmail.com>
2024-10-13 04:30:49 -0400

    Added navbar to add-ingredients

--- client/app/add-ingredients/page.tsx
+++ client/app/add-ingredients/page.tsx
@@ -2,8 +2,38 @@
 
 import React, { useState } from 'react';
 import './IngredientInput.css'; // Import for simple styling
+import Link from 'next/link'; // Import Link from next/link
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+
+const pages = [
+  { name: 'Add Ingredients', href: '/add-ingredients' },
+  { name: 'Meal Calc', href: '/meal' },
+  { name: 'Logout', href: '/login' }
+];
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+
 
 const IngredientInput: React.FC = () => {
+  
   const [ingredient, setIngredient] = useState<string>(''); // State for the current input
   const [ingredientsList, setIngredientsList] = useState<string[]>([]); // State for the list of ingredients
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(true); // Open dropdown by default
@@ -57,47 +87,189 @@ const IngredientInput: React.FC = () => {
     
   };
 
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElNav(event.currentTarget);
+  };
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+      setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+  };
+
   return (
-    <div className="ingredient-input-container">
-      <div className="left-menu">
-        <div className="input-section">
-          <h3>Enter Ingredient:</h3>
-          <div className="input-box">
-            <input
-              type="text"
-              value={ingredient}
-              onChange={handleInputChange}
-              onKeyDown={handleKeyPress} // Add this to handle Enter key
-              placeholder="Type an ingredient"
-            />
-            <button onClick={handleAddIngredient}>+</button>
+    <>
+    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+      <AppBar position="static" sx={{ 
+        width: '100%', 
+        backgroundColor: 'lightgray', 
+        boxShadow: 'none' }}>
+                    <Container maxWidth={false}>
+                        <Toolbar disableGutters sx={{ padding: 0 }}>
+                        {/* <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} /> */}
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/home"
+                            sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            }}
+                        >
+                             Nutrition Planner
+                        </Typography>
+                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleOpenNavMenu}
+                                    color="inherit"
+                                >
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorElNav}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'left',
+                                    }}
+                                    open={Boolean(anchorElNav)}
+                                    onClose={handleCloseNavMenu}
+                                    sx={{ display: { xs: 'block', md: 'none' } }}
+                                >
+                                    {pages.map((page) => (
+                                        <MenuItem key={page.name} onClick={handleCloseNavMenu}>
+                                            <Link href={page.href} style={{ textDecoration: 'none', color: 'inherit' }}>
+                                                <Typography sx={{ textAlign: 'center' }}>{page.name}</Typography>
+                                            </Link>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                            <Typography
+                                variant="h5"
+                                noWrap
+                                component="a"
+                                href="#app-bar-with-responsive-menu"
+                                sx={{
+                                mr: 2,
+                                display: { xs: 'flex', md: 'none' },
+                                flexGrow: 1,
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'inherit',
+                                textDecoration: 'none',
+                                }}
+                            >
+                                Nutrition Planner
+                            </Typography>
+                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                                {pages.map((page) => (
+                                    <Link key={page.name} href={page.href} style={{ textDecoration: 'none' }}>
+                                        <Button sx={{ my: 2, color: 'black', display: 'block' }}>
+                                            {page.name}
+                                        </Button>
+                                    </Link>
+                                ))}
+                            </Box>
+
+                            <Box sx={{ flexGrow: 0 }}>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                        <Avatar alt="" src="/static/images/avatar/2.jpg" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: '100%' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    {settings.map((setting) => (
+                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                            <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </Box>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+                </Box>            
+      <div className="ingredient-input-container">
+        <div className="left-menu">
+          <div className="input-section">
+            <h3>Enter Ingredient:</h3>
+            <div className="input-box">
+              <input
+                type="text"
+                value={ingredient}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyPress} // Add this to handle Enter key
+                placeholder="Type an ingredient"
+              />
+              
+            </div>
           </div>
-        </div>
 
-        {isDropdownOpen && (
-          <ul className="dropdown-list">
-            {ingredientsList.map((ing, index) => (
-              <li key={index} className="ingredient-item">
-                {ing}
-                <button
-                  className="remove-button"
-                  onClick={() => handleRemoveIngredient(index)}
-                >
-                  ✖
-                </button>
-              </li>
-            ))}
-          </ul>
-        )}
+          {isDropdownOpen && (
+            <ul className="dropdown-list">
+              {ingredientsList.map((ing, index) => (
+                <li key={index} className="ingredient-item">
+                  {ing}
+                  <button
+                    className="remove-button"
+                    onClick={() => handleRemoveIngredient(index)}
+                  >
+                    ✖
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
 
-        {/* Use a form with onSubmit to call handleSubmit */}
-        <form id="ingredient_form" onSubmit={handleSubmit}>
-         
-            <button type = "submit" className = "submit-button"> <a href = "/Meal" target = "_self" rel = "noopener" style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>Submit</a></button>
+          {/* Use a form with onSubmit to call handleSubmit */}
+          <form id="ingredient_form" onSubmit={handleSubmit}>
           
-        </form>
+              <button type = "submit" className = "submit-button"> <a href = "/meal" target = "_self" rel = "noopener" style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>Submit</a></button>
+            
+          </form>
+        </div>
       </div>
-    </div>
+      </>
   );
 };
 
