245a8af1c197f84a699f03fc522793ea78f821ef
Leo <leoeatsgrass@gmail.com>
2024-10-12 20:05:37 -0400

    Pages edit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,13 +3,16 @@ import * as React from 'react';
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
 
+
 export default function Home() {
   return( <div>
+    <h1 style = {{textAlign:'center'}}>Title</h1>
     <Box
       component = "form"
       sx = {{ '& >:not(style)':{m:1, width: '50ch'}}}
       noValidate
       autoComplete = "off"
+      style = {{textAlign:'center'}}
       ><TextField id="standard-basic" label="What ingredients do you have" variant="standard" /></Box>
 
   </div>
